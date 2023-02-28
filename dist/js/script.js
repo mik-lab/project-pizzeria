b2f2478d956d7a9491ddd47800f9ac887d1e50e0
Mikołaj <121145556+mik-lab@users.noreply.github.com>
2023-02-28 23:28:40 +0100

    Add instances for all products

--- dist/js/script.js
+++ dist/js/script.js
@@ -62,8 +62,18 @@
 
   const app = {
     initMenu: function(){
-      const testProduct = new Product();
-      console.log('testProduct', testProduct);
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource; 
     },
 
     init: function(){
@@ -74,8 +84,9 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
 
+      thisApp.initData();
       thisApp.initMenu();
-    },
+    }
   };
 
   app.init();
